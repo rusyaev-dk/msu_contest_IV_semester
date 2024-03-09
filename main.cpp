e3d57696d98993f67470cc33c19977e4c89649c0
Dnrvv_asus <rusyaev.dk@gmail.com>
2024-03-09 10:48:36 +0500

    Added base files.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include "Set.h"
+
 #include "MainMemoryManager.h"
 #include <iostream>
 
 int main() {
     MainMemoryManager main_mem_mamanger(100);
-    Set my_set(main_mem_mamanger);
+    
     return 0;
 }
