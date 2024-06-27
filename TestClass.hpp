a5398427f265ecbbb654e3823c18967892de2f8b
V4nadiy <vadim.rez2016@gmail.com>
2024-06-27 22:16:46 +0500

    finally

--- TestClass.hpp
+++ TestClass.hpp
@@ -7,20 +7,53 @@ using namespace std;
 
 
 class ListTest{
+    private:
+    List* test_list;
+
     public:
-    List* create_random_elements(int num){
-        time_t start = clock();
-        int memory = 10*num;
-        Mem mem(memory);
-        int new_elem;
-        List created_list(mem);
-        for(int i = 0; i < num ; i++){
-            new_elem = 5;
-            created_list.push_front(&new_elem , sizeof(new_elem));
+
+        int create_some_random_elements(int num , int range){
+            time_t start = clock();
+            int memory = 10*num;
+            Mem mem(memory);
+            int new_elem;
+            test_list = new List(mem);
+
+            for(int i = 0 ; i < num ; i++){
+                new_elem = i;
+                test_list->push_front(&new_elem , sizeof(new_elem));
+            }
+
+            time_t stop = clock();
+            float overall_time = (stop-start)*1.0/CLOCKS_PER_SEC;
+            cout << test_list->size() << endl;
+            cout << "Time of creating: " <<  overall_time << " sec" << endl;
         }
-        time_t stop = clock();
-        float overall_time = (stop-start)*1.0/CLOCKS_PER_SEC;
-        cout << "Time of creating: " <<  overall_time << " sec" << endl;
-        return &created_list;
-    }  
+
+
+        int delete_evens(){
+            time_t start = clock();
+            cout << test_list->size() << endl;
+            Container::Iterator* new_iter = test_list->newIterator();
+            size_t size;
+            int buf;
+            int start_size = test_list->size();
+            while(new_iter->hasNext()){
+                buf = *(int*)new_iter->getElement(size);
+                if(buf%2==0){
+                    test_list->remove(new_iter);
+                }
+                else{
+                    new_iter->goToNext();
+                }
+            }
+            
+            cout << "Elements deleted: " << start_size - test_list->size() << endl;
+
+            time_t stop = clock();
+            float overall_time = (stop-start)*1.0/CLOCKS_PER_SEC;
+            cout << "Time of creating: " <<  overall_time << " sec" << endl;
+        }
+
+    
 };
--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #include "LinkedList1.hpp"
 #include "./Mem.h"
 #include <ctime>
-#define MILL 500
+#define MILL 100000
 #include "TestClass.hpp"
 
 using namespace std;
@@ -10,29 +10,35 @@ using namespace std;
 
 
 int main(){
-    time_t start = clock();
-    int memory = 10*MILL;
-    Mem mem(memory);
-    int new_elem;
-    List created_list(mem);
-    for(int i = 0; i < MILL ; i++){
-        new_elem = i;
-        created_list.push_front(&new_elem , sizeof(new_elem));
-    }
-    time_t stop = clock();
-    float overall_time = (stop-start)*1.0/CLOCKS_PER_SEC;
-    cout << "Time of creating: " <<  overall_time << " sec" << endl;
-    cout << created_list.size() << endl;
-    new_elem = 5;
-    size_t buf;
-    Container::Iterator* new_iter;
-    for(int i = 0 ; i < MILL; i++ ){
-        if(i%2){
-            new_iter = created_list.find(&i , sizeof(i));
-            if(new_iter){
-                created_list.remove(new_iter);
-            }
-        }
-    }
-    cout << created_list.size() << endl;
+
+    // time_t start = clock();
+    // int memory = 10*MILL;
+    // Mem mem(memory);
+    // int new_elem;
+    // List created_list(mem);
+    // for(int i = 0; i < MILL ; i++){
+    //     new_elem = i;
+    //     created_list.push_front(&new_elem , sizeof(new_elem));
+    // }
+    // cout << created_list.size() << endl;
+    // new_elem = 5;
+    // size_t buf;
+    // Container::Iterator* new_iter;
+    // for(int i = 0 ; i < MILL; i++ ){
+    //     if(i%2){
+    //         new_iter = created_list.find(&i , sizeof(i));
+    //         // if(new_iter){
+    //         //     created_list.remove(new_iter);
+    //         // }
+    //     }
+    // }
+    // cout << created_list.size() << endl;
+    // created_list.clear();
+    // cout << created_list.size() << endl;
+    // time_t stop = clock();
+    // float overall_time = (stop-start)*1.0/CLOCKS_PER_SEC;
+    // cout << "Time of creating: " <<  overall_time << " sec" << endl;
+    ListTest* new_test = new ListTest();
+    new_test->create_some_random_elements(1000 , 10);
+    new_test->delete_evens();
 }
